0967be9617d7203fc2b12c3dfc054badffe097ff
Kirill Smekin <smekin.kir@yandex.ru>
2023-10-16 14:11:02 +0300

    Красивая картинка

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ class App():
     def __init__(self) -> None:
         pg.init()
         self.sc = pg.display.set_mode((W, H))
+        pg.display.set_caption("1XВШЭ")
+        pg.display.set_icon(pg.image.load("images/HSE_icon.png").convert())
         self.clock = pg.time.Clock()
+        self.intro = pg.image.load("images/1XHSE.jpg").convert()
     def run(self):
         while 1:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
                     exit()
-            self.sc.fill((0, 0, 0))
+            self.sc.blit(self.intro, (0, 0))
             pg.display.flip()
             self.clock.tick(60)
