56a8c3e2a4231ac4106ccd0a2a521dbabf95f10f
Elesin <elesin.alex2017@yandex.ru>
2023-10-16 19:41:47 +0300

    обновил как просил Кирюха

--- events_generation.py
+++ events_generation.py
@@ -1,13 +1,17 @@
 import random
 
 class SportGen:
-    def __init__(self, sports):
+    def __init__(self, adjectives, sports):
+        self.adjectives = adjectives
         self.sports = sports
 
     def generate_random_sport(self):
-        return random.choice(self.sports)
+        random_adjective = random.choice(self.adjectives)
+        random_sport = random.choice(self.sports)
+        return f"{random_adjective} {random_sport}"
 
-sports_list = ["Футбол","Хоккей", "Хоббихорсинг", "Дота 2", "CS2"]
-sports_generator = SportGen(sports_list)
+adjectives_list = ["Пьяный", "Польский", "Юношеский", "Подводный", "Арабский"]
+sports_list = ["футбол","хоккей", "хоббихорсинг", "баскетбол ногой", "CS"]
+sports_generator = SportGen(adjectives_list, sports_list)
 random_sport = sports_generator.generate_random_sport()
 print("Спорт: " + random_sport)
